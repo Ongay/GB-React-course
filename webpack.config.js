82024ed7f28dcebe4600b9f7c8d309d9a7743568
Onai <61268902+Ongay@users.noreply.github.com>
2021-04-29 17:47:24 +0600

    Заглушка
    Это заглушка. Завершенное ДЗ сдам в ближайшее время

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './index.js',
+    },
+    context: path.resolve(__dirname, "static_src"),
+    output: {
+        path: path.resolve(__dirname, "static", "build"),
+        filename: 'app.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "static", "build"),
+        port: 8080,
+        open: true,
+    }
+};
